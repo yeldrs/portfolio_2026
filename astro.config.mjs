a0515eeb83cb832ad98bc5b2fff4ae901a24f802
yeldrs <yass.elidrissi97@gmail.com>
2025-12-04 22:42:53 +0100

    Add CNAME for custom domain and merge deploy/test

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,8 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 
 export default defineConfig({
+  output: 'static', 
+  // Base est vide si vous déployez à la racine du domaine
+  base: '/',
   integrations: [tailwind(), mdx()],
 });
